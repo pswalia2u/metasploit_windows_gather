668f9041e6f99d4bdeb68a926d50bddfa37e0295
Prabhsimran <pswalia2u@gmail.com>
2020-04-26 20:16:03 +0530

    another

---
+++ enum_termserv.ps1
@@ -0,0 +1,4 @@
+ Function enum_term_serv
+ {
+ Get-ItemProperty  -Path HKLM:\SYSTEM\CurrentControlSet\Control\Termin*Server\WinStations\RDP*CP\ 
+ }
--- enum_trusted_locations.ps1
+++ enum_trusted_locations.ps1
@@ -7,11 +7,24 @@ function enum_versions
     }
 }
 
-function applications($version) 
+function applications($version)
 {
-    $version=15.0
     $app=Get-ChildItem Get-Item -Path "HKCU:\SOFTWARE\Microsoft\Office\$version\*" | Select-Object PsChildName
     $app
 }
 
-Get-ChildItem -path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Word\Security\Trusted Locations"
+
+Function enum_trusted_locations1 ($ver,$app_name)
+{
+    Write-Output $ver
+    Write-Output $app_name
+    if(Test-Path("HKCU:\SOFTWARE\Microsoft\Office\$ver\$app_name\Security\Trusted Locations"))
+    {
+        Get-ChildItem -path "HKCU:\SOFTWARE\Microsoft\Office\$ver\$app_name\Security\Trusted Locations"
+    }
+    else
+    {
+        Write-Output "No trusted locations exists."
+    }
+}
+enum_trusted_locations1 "16.0" "Word"
